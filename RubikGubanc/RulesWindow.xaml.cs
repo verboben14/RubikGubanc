ceeb537fbfa23dd81534b6987025ef4c6b9917a6
verboben14 <verboben14@VERBO-PC>
2016-10-17 01:56:28 +0200

    RulesWindow alapok

--- RubikGubanc/RulesWindow.xaml.cs
+++ RubikGubanc/RulesWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace RubikGubanc
         public RulesWindow()
         {
             InitializeComponent();
+            rulesTxb.Text = "Ez a program a Rubik Gubanc nevű játék egy szimulációs megoldója.\n\nA Rubik Gubanc 9 kis habkarton lapból álló logikai játék. Ezek a kartonlapok kétoldalasak, tehát összesen 18 db ábránk van. Minden ábrán 4 db különböző színű fonal látható, ami egymásba van gabalyodva. Minden ábra ugyanúgy néz ki, azzal a különbséggel, hogy más a fonalak színezése. A játék célja, hogy úgy helyezzük le ezt a kilenc kartonlapocskát, hogy a fonalak színezése folyamatos legyen, és az egész egy 3x3-as négyzetet alkosson. A kártyákat összesen 95 126 813 710 féleképpen lehet lerakni, amiből 1 a jó megoldás. Méretek: A lapok egyenként 6.5 x 6.5 cm, míg a 3x3-as négyzet kirakva 19.5 x 19.5 cm nagyságú.";
+            rulesTxb.TextWrapping = TextWrapping.Wrap;
         }
 
         private void BackClick(object sender, RoutedEventArgs e)
